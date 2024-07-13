81fc0ce7308ceb474fd9f2a6514b4eb87a6bf4bf
Buttars <17345308+Buttars@users.noreply.github.com>
2024-07-12 22:05:39 -0600

    refactor: extract user from hosts/desktop to standalone users directory

--- users/buttars/default.nix
+++ users/buttars/default.nix
@@ -15,18 +15,18 @@
 
     home.file.".config/nvim" =
       {
-        source = ../../../config/nvim;
+        source = config/nvim;
         recursive = true;
       };
 
-    home.file.".config/hypr".source = ../../../config/hypr;
-    home.file.".config/tmux".source = ../../../config/tmux;
-    home.file.".config/lf".source = ../../../config/lf;
-    home.file.".config/zsh".source = ../../../config/zsh;
-    home.file.".config/alacritty".source = ../../../config/alacritty;
-    home.file.".config/rofi".source = ../../../config/rofi;
-    home.file.".config/waybar".source = ../../../config/waybar;
-    home.file.".zprofile".source = ../../../config/shell/profile;
+    home.file.".config/hypr".source = config/hypr;
+    home.file.".config/tmux".source = config/tmux;
+    home.file.".config/lf".source = config/lf;
+    home.file.".config/zsh".source = config/zsh;
+    home.file.".config/alacritty".source = config/alacritty;
+    home.file.".config/rofi".source = config/rofi;
+    home.file.".config/waybar".source = config/waybar;
+    home.file.".zprofile".source = config/shell/profile;
     home.file.".config/nixpkgs/config.nix" = {
       text = ''
         {
