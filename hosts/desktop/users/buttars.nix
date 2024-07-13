0a65e0120e2aab28144503229d4f5f344ff0acc7
Buttars <17345308+Buttars@users.noreply.github.com>
2024-07-12 21:53:49 -0600

    refactor(host/desktop): load nvim config files recursively

--- hosts/desktop/users/buttars.nix
+++ hosts/desktop/users/buttars.nix
@@ -1,4 +1,4 @@
-{ config, ... }: {
+{ config, lib, home-manager, ... }: {
   users.users.buttars = {
     isNormalUser = true;
     extraGroups = [ "wheel" "networkmanager" ];
@@ -6,22 +6,27 @@
   };
 
 
-  home-manager.users.buttars = { ... }: {
+  home-manager.users.buttars = { config, ... }: {
     home.stateVersion = "22.05";
 
     home.sessionVariables = {
       BROWSER = "brave";
     };
 
+    home.file.".config/nvim" =
+      {
+        source = ../../../config/nvim;
+        recursive = true;
+      };
+
     home.file.".config/hypr".source = ../../../config/hypr;
     home.file.".config/tmux".source = ../../../config/tmux;
     home.file.".config/lf".source = ../../../config/lf;
     home.file.".config/zsh".source = ../../../config/zsh;
-    home.file.".zprofile".source = ../../../config/shell/profile;
     home.file.".config/alacritty".source = ../../../config/alacritty;
     home.file.".config/rofi".source = ../../../config/rofi;
     home.file.".config/waybar".source = ../../../config/waybar;
-    home.file.".config/nvim".source = ../../../config/nvim;
+    home.file.".zprofile".source = ../../../config/shell/profile;
     home.file.".config/nixpkgs/config.nix" = {
       text = ''
         {
