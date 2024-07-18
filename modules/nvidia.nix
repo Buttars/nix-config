e43a788b712a638db9386c2de1e0ae59199dd61f
Buttars <17345308+Buttars@users.noreply.github.com>
2024-07-18 12:06:34 -0600

    feat(modules/nvidia): set to experimental nvidia driver 555.82

--- modules/nvidia.nix
+++ modules/nvidia.nix
@@ -16,6 +16,7 @@ in
     # Load nvidia driver for Xorg and Wayland
     services.xserver.videoDrivers = [ "nvidia" ];
 
+
     hardware.nvidia = {
 
       # Modesetting is required.
@@ -41,7 +42,18 @@ in
       nvidiaSettings = true;
 
       # Optionally, you may need to select the appropriate driver version for your specific GPU.
-      package = config.boot.kernelPackages.nvidiaPackages.stable;
+      # package = config.boot.kernelPackages.nvidiaPackages.stable;
+
+      package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+        version = "555.58";
+
+        sha256_64bit = "sha256-bXvcXkg2kQZuCNKRZM5QoTaTjF4l2TtrsKUvyicj5ew=";
+        sha256_aarch64 = lib.fakeSha256;
+        openSha256 = lib.fakeSha256;
+        settingsSha256 = "sha256-vWnrXlBCb3K5uVkDFmJDVq51wrCoqgPF03lSjZOuU8M=";
+        persistencedSha256 = lib.fakeSha256;
+      };
+
 
       #prime = { 
       ## Make sure to use the correct Bus ID values for your system!
