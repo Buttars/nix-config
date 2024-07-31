716def8ea68fab45f916fbd638b77c59b5749314
Buttars <17345308+Buttars@users.noreply.github.com>
2024-07-30 22:56:13 -0600

    feat(profiles): add virtualization profile

--- profiles/virtualization.nix
+++ profiles/virtualization.nix
@@ -1,4 +1,21 @@
 { pkgs, ... }: {
+  virtualisation.libvirtd = {
+    enable = true;
+    qemu = {
+      package = pkgs.qemu_kvm;
+      runAsRoot = true;
+      ovmf = {
+        enable = true;
+        packages = [
+          (pkgs.OVMF.override {
+            secureBoot = true;
+            tpmSupport = true;
+          }).fd
+        ];
+      };
+    };
+  };
+
   environment.systemPackages = with pkgs; [
     virt-manager
     qemu
