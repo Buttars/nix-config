fb9066377c25a72b71abfcf271c241681bae288a
Buttars <17345308+Buttars@users.noreply.github.com>
2024-08-10 21:21:32 -0600

    fix(modules/xremap): xremap default enabled option breaking builds

--- modules/xremap.nix
+++ modules/xremap.nix
@@ -8,19 +8,24 @@ in
   };
 
 
-  config = lib.mkIf cfg.enable {
-    services.xremap.withWlroots = true;
-    services.xremap.config.modmap = [
-      {
-        name = "Global";
-        remap = {
-          "CapsLock" = {
-            held = "WIN_L";
-            alone = "ESC";
-            alone_timeout_milis = 500;
+  config = lib.mkMerge [
+    {
+      services.xremap.enable = false;
+    }
+    (lib.mkIf cfg.enable {
+      services.xremap.withWlroots = true;
+      services.xremap.config.modmap = [
+        {
+          name = "Global";
+          remap = {
+            "CapsLock" = {
+              held = "WIN_L";
+              alone = "ESC";
+              alone_timeout_milis = 500;
+            };
           };
-        };
-      }
-    ];
-  };
+        }
+      ];
+    })
+  ];
 }
