c733d3dbad8f7dbdb2ee0c98fdd694b83ead3c43
Buttars <17345308+Buttars@users.noreply.github.com>
2024-11-03 01:13:05 -0700

    fix(profiles/gtk): fix some ui scaling issues

--- profiles/gtk.nix
+++ profiles/gtk.nix
@@ -24,11 +24,11 @@ in
       GTK_THEME = "Arc-Dark";
       QT_QPA_PLATFORMTHEME = "qt5ct";
       QT_STYLE_OVERRIDE = "gtk2";
-      GDK_DPI_SCALE = "1.5";
-      GTK_DPI_SCALE = "1.5";
-      QT_FONT_DPI = "144";
-      GDK_SCALE = "1.5";
-      QT_SCALE_FACTOR = "1.5";
+      GDK_DPI_SCALE = "1";
+      GTK_DPI_SCALE = "1";
+      QT_FONT_DPI = "81";
+      GDK_SCALE = "1";
+      QT_SCALE_FACTOR = "1";
       XCURSOR_THEME = "Bibata-Modern-Ice";
       XCURSOR_SIZE = "20";
       GTK_ICON_THEME = "Papirus-Dark";
