107846eece61112db3c975bd6d3191a694b3aa13
Buttars <17345308+Buttars@users.noreply.github.com>
2024-12-11 22:27:16 -0700

    refactor(hosts): migrate all host definitions to use mkNixos

--- hosts/default.nix
+++ hosts/default.nix
@@ -1,34 +1,29 @@
 { nixpkgs, nixosModule, inputs, stateVersion, ... }:
 let
-  sys = system: mods: nixpkgs.lib.nixosSystem {
-    inherit system;
-    modules = [
-      {
-        _module.args = inputs;
-        nixpkgs = {
-          overlays = [
-            inputs.self.overlays.additions
-            inputs.self.overlays.modifications
-            inputs.self.overlays.unstable-packages
-          ];
-        };
-      }
-      nixosModule
-      ../profiles/common.nix
-    ] ++ mods;
-    extraModules = [ ];
-    specialArgs = { inherit inputs system; };
-  };
   helper = import ../lib/helpers.nix { inherit inputs nixosModule stateVersion; };
 in
 {
-  vm = sys "x86_64-linux" [ ./nixos/vm ../hardware/vm/hardware-configuration.nix ];
-  wsl = sys "x86_64-linux" [ ./nixos/vm ../hardware/wsl/hardware-configuration.nix];
-  field-computer = sys "x86_64-linux" [ ./nixos/field-computer ];
-  laptop = sys "x86_64-linux" [ ./nixos/laptop ];
-  desktop = sys "x86_64-linux" [ ./nixos/desktop ];
-  portainer = sys "x86_64-linux" [ ./nixos/portainer ];
-  desktop-experimental = helper.mkNixos {
+  wsl = helper.mkNixos {
+    hostname = "vm";
+    username = "vm";
+    modules = [ ./nixos/vm ];
+  };
+  field-computer = helper.mkNixos {
+    hostname = "field-computer";
+    username = "field-computer";
+    modules = [ ./nixos/field-computer ];
+  };
+  laptop = helper.mkNixos {
+    hostname = "laptop";
+    username = "buttars";
+    modules = [ ./nixos/laptop ];
+  };
+  portainer = helper.mkNixos {
+    hostname = "portainer";
+    username = "portainer";
+    modules = [ ./nixos/portainer-servus ];
+  };
+  desktop = helper.mkNixos {
     hostname = "buttars-desktop";
     username = "buttars";
     modules = [./nixos/desktop];
