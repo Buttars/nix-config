b79f4182e89dc3f8c33fa6b78dd9f7cc9afbe577
Buttars <17345308+Buttars@users.noreply.github.com>
2025-01-13 18:27:42 -0700

    feat: add buttars user and configure networking for buttars-desktop

--- hosts/nixos/buttars-desktop/default.nix
+++ hosts/nixos/buttars-desktop/default.nix
@@ -1,5 +1,14 @@
 { ... }: {
   imports = [
     ./hardware-configuration.nix
+
+    ../common/core
+    ../common/users/buttars
   ];
+
+  networking = {
+    hostName = "buttars-desktop";
+    useDHCP = true;
+    networkmanager.enable = true;
+  };
 }
