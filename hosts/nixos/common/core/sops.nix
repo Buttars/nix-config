5fea8facf1c26fbc6a26c39168bc17735092c123
Landon Buttars <17345308+Buttars@users.noreply.github.com>
2025-02-07 18:46:11 -0700

    fix: typo

--- hosts/nixos/common/core/sops.nix
+++ hosts/nixos/common/core/sops.nix
@@ -8,7 +8,7 @@
     validateSopsFiles = false;
 
     age = {
-      sshKeyPaths = [ "/ets/ssh/ssh_host_ed25519_key" ];
+      sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
       keyFile = "/var/lib/sops-nix/key.txt";
       generateKey = true;
     };
