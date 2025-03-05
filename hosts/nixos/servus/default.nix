bdfc323e2728c6c84e6d582a78b20e92753c975c
Landon Buttars <17345308+Buttars@users.noreply.github.com>
2025-03-05 01:25:58 -0700

    tmp

---
+++ hosts/nixos/servus/default.nix
@@ -0,0 +1,20 @@
+{ hostname, ... }: {
+  networking = {
+    hostName = hostname;
+    networkmanager.enable = true;
+    firewall.enable = true;
+
+    interfaces = {
+      ens18 = {
+        ipv4.addresses = [
+          {
+            address = "10.0.1.2";
+            prefixLength = 16;
+          }
+        ];
+      };
+    };
+
+    defaultGateway = "10.0.0.1";
+  };
+}
