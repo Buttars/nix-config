6dcf37f13396a02473cdc5a94b4e353366b998b5
Buttars <17345308+Buttars@users.noreply.github.com>
2025-03-18 14:27:18 -0600

    refactor(hosts/darwin): replace implicit features with shared core/common features

--- hosts/darwin/N4FQ62JR/configuration.nix
+++ hosts/darwin/N4FQ62JR/configuration.nix
@@ -2,64 +2,9 @@
 {
 
   imports = [
+    ../common/users/landon.buttars
     ./system.nix
   ];
-
-  # List packages installed in system profile. To search by name, run:
-  # $ nix-env -qaP | grep wget
-  environment.systemPackages = with pkgs; [
-    #libgcc
-    bat
-    cargo
-    delta
-    devenv
-    devpod
-    fd
-    fuse
-    gcc
-    git
-    gnumake
-    go
-    gotop
-    lazydocker
-    luajitPackages.luarocks-nix
-    neovim
-    nettools
-    nixpkgs-fmt
-    nodejs
-    python3
-    ripgrep
-    sshfs
-    tmux
-    unzip
-    fzf
-  ];
-
-  programs.direnv.enable = true;
-
-  fonts.packages = with pkgs; [
-    nerd-fonts.sauce-code-pro
-    nerd-fonts.noto
-    nerd-fonts.inconsolata
-    nerd-fonts.roboto-mono
-    nerd-fonts.commit-mono
-    # (nerdfonts.override { fonts = [ "SourceCodePro" "Noto" "Inconsolata" "RobotoMono" "CommitMono" ]; })
-  ];
-
-  users.users."landon.buttars" = {
-    home = "/Users/landon.buttars";
-  };
-
-  home-manager.users."landon.buttars" = import ./home.nix { inherit pkgs dotfiles; };
-
-  # Auto upgrade nix package and the daemon service.
-  #services.nix-daemon.enable = true;
-  # nix.package = pkgs.nix;
-
-  # Necessary for using flakes on this system.
-  nix.settings.experimental-features = "nix-command flakes";
-
-  programs.zsh.enable = true;
 
   nix.channel.enable = false;
 
@@ -68,6 +13,11 @@
     "landon.buttars"
     "@wheel"
   ];
+
+  nix.gc.automatic = false;
+  nix.optimise.automatic = false;
+
+  nix.enable = false;
 
   # Used for backwards compatibility, please read the changelog before changing.
   system.stateVersion = 5;
