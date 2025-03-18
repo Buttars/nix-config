8004a969d567a85869d895672afd5de10802201a
Landon Buttars <17345308+Buttars@users.noreply.github.com>
2025-03-18 14:27:18 -0600

    refactor(hosts/darwin): replace implicit features with shared core/common features

---
+++ hosts/darwin/common/users/landon.buttars/default.nix
@@ -0,0 +1,26 @@
+{ inputs, pkgs, lib, config, ... }:
+let
+  ifGroupsExist = groups: builtins.filter (group: builtins.hasAttr group config.users.groups) groups;
+in
+{
+  users.users."landon.buttars" = {
+    home = "/Users/landon.buttars/";
+    # shell = pkgs.fish;
+
+    # openssh.authorizedKeys.keys = [
+    #   (builtins.readFile ./keys/id_ed25519.pub)
+    # ];
+  };
+
+  environment.systemPackages = [ pkgs.home-manager ];
+
+  home-manager.backupFileExtension = "backup";
+
+  home-manager.extraSpecialArgs = {
+    inherit inputs;
+  };
+
+  home-manager.useGlobalPkgs = true;
+
+  home-manager.users."landon.buttars" = import ../../../../../home/wgu/N4FQ62JR4D.nix;
+}
