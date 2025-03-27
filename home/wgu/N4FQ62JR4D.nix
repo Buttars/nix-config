350f4f59b5b451b0136824973486d1ea20e46b94
Buttars <17345308+Buttars@users.noreply.github.com>
2025-03-27 13:16:41 -0600

    feat(home/wgu): add browser feature to wgu config

--- home/wgu/N4FQ62JR4D.nix
+++ home/wgu/N4FQ62JR4D.nix
@@ -1,8 +1,14 @@
-{ ... }: {
+{ pkgs, ... }: {
   imports = [
     ./common/core
     ../common/features/programming.nix
     ../common/features/terminal-emulator.nix
     ../common/features/taskwarrior.nix
   ];
+
+  home.packages = with pkgs; [
+    google-chrome
+    firefox
+  ];
+
 }
