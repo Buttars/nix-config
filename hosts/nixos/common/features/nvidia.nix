03c001672ae45ad915c919250ae63da0b3fc16f3
Landon Buttars <17345308+Buttars@users.noreply.github.com>
2025-03-31 22:11:52 -0600

    feat(hosts/nixos): configure nvidia drivers

--- hosts/nixos/common/features/nvidia.nix
+++ hosts/nixos/common/features/nvidia.nix
@@ -1,12 +1,16 @@
-{
+{ config, ... }: {
   hardware.graphics = {
     enable = true;
   };
+
+  services.xserver.videoDrivers = [ "nvidia" ];
 
   hardware.nvidia = {
     modesetting.enable = true;
     powerManagement.enable = false;
     powerManagement.finegrained = false;
     nvidiaSettings = true;
+    open = true;
+    package = config.boot.kernelPackages.nvidiaPackages.production;
   };
 }
