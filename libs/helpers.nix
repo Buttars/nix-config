fd8342328dee5784272ef3e38288f5ba00457a92
Landon Buttars <17345308+Buttars@users.noreply.github.com>
2025-04-25 00:38:19 -0600

    feat(libs): add mkDarwin helper

--- libs/helpers.nix
+++ libs/helpers.nix
@@ -1,15 +1,14 @@
-{
-  inputs,
-  nixosModule,
-  stateVersion,
-  ...
+{ inputs
+, nixosModule
+, stateVersion
+, ...
 }:
 {
   mkHome =
-    {
-      hostname,
-      username ? "buttars",
-      platform ? "x86_64-linux",
+    { hostname
+    , username ? "buttars"
+    , platform ? "x86_64-linux"
+    ,
     }:
     inputs.home-manager.lib.homeManagerConfiguration {
       pkgs = inputs.nixpkgs.legacyPackages.${platform};
@@ -25,13 +24,47 @@
       modules = [ ../home-manager ];
     };
 
+  mkDarwin =
+    { hostname
+    , username
+    , platform ? "aarch64-darwin"
+    , modules ? [ ]
+    , systemConfiguration ? inputs.darwin.lib.darwinSystem
+    }:
+    systemConfiguration {
+      modules = [
+        {
+          _module.args = inputs;
+          nixpkgs = {
+            overlays = [
+              inputs.self.overlays.additions
+              inputs.self.overlays.modifications
+              inputs.self.overlays.unstable-packages
+            ];
+          };
+        }
+        nixosModule
+        ../hosts/nixos/common/core/default.nix
+      ] ++ modules;
+      specialArgs = {
+        inherit
+          inputs
+          hostname
+          platform
+          stateVersion
+          ;
+
+        lib = inputs.nixpkgs.lib.extend (self: super: { custom = import ./libs { inherit (inputs.pkgs) lib; }; });
+      };
+    };
+
   mkNixos =
-    {
-      hostname,
-      username ? "buttars",
-      platform ? "x86_64-linux",
-      modules ? [ ],
-      systemConfiguration ? inputs.nixpkgs.lib.nixosSystem,
+    { hostname
+    , username ? "buttars"
+    , platform ? "x86_64-linux"
+    , modules ? [ ]
+    , systemConfiguration ? inputs.nixpkgs.lib.nixosSystem
+    ,
     }:
     systemConfiguration {
       modules = [
