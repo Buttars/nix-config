669cd4e21d2215631ab7f408a420c3d713630378
Landon Buttars <17345308+Buttars@users.noreply.github.com>
2025-04-25 00:26:00 -0600

    feat(home/wgu): add fish shell config

--- home/wgu/N4FQ62JR4D.nix
+++ home/wgu/N4FQ62JR4D.nix
@@ -1,9 +1,11 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+{
   imports = [
     ./common/core
     ../common/features/programming.nix
     ../common/features/terminal-emulator.nix
     ../common/features/taskwarrior.nix
+    ../common/features/cli/fish.nix
   ];
 
   home.packages = with pkgs; [
