4a123ae12f8b1e53403aa283e64730bd28e27d77
Landon Buttars <17345308+Buttars@users.noreply.github.com>
2025-04-28 00:18:17 -0600

    feat(host/servus): increase tor bandwidth

--- hosts/nixos/servus/tor-relay.nix
+++ hosts/nixos/servus/tor-relay.nix
@@ -15,7 +15,7 @@
       Nickname = "Buttars";
       ORPort = 9001;
       ControlPort = 9051;
-      BandWidthRate = "1 MBytes";
+      BandWidthRate = "1 GBytes";
       HashedControlPassword = "16:D36722B1FEFD5498600A2A5837B7578F4FD840EA0715D8F4D6BC20D94E";
     };
   };
