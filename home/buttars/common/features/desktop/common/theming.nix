f4437b19621e60927d4331cae5cc26bc41a10505
Landon Buttars <17345308+Buttars@users.noreply.github.com>
2025-04-29 21:45:55 -0600

    feat(home/buttars): override purple with red because purple is disgusting

--- home/buttars/common/features/desktop/common/theming.nix
+++ home/buttars/common/features/desktop/common/theming.nix
@@ -23,13 +23,15 @@
   stylix.enable = true;
   # stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/rebecca.yaml";
 
-  stylix.base16Scheme = builtins.fetchurl {
-    url = "https://raw.githubusercontent.com/scottmckendry/cyberdream.nvim/main/extras/base16/cyberdream.yaml";
-    sha256 = "1bfi479g7v5cz41d2s0lbjlqmfzaah68cj1065zzsqksx3n63znf";
-  };
-
-  stylix.override = {
-    base00 = "#0F0F11";
+  stylix = {
+    base16Scheme = builtins.fetchurl {
+      url = "https://raw.githubusercontent.com/scottmckendry/cyberdream.nvim/main/extras/base16/cyberdream.yaml";
+      sha256 = "1bfi479g7v5cz41d2s0lbjlqmfzaah68cj1065zzsqksx3n63znf";
+    };
+    override = {
+      base00 = "#0F0F11";
+      base0E = "#DE4F72";
+    };
   };
 
   stylix.fonts = {
