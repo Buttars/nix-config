0330bb9eb1976fde75fc5361b04b9e135418186c
Landon Buttars <17345308+Buttars@users.noreply.github.com>
2025-05-02 18:59:36 -0600

    feat(hosts/servus): add torrent media management services

---
+++ hosts/nixos/servus/torrent-media-management.nix
@@ -0,0 +1,16 @@
+{ ... }: {
+  services.radarr = {
+    enable = true;
+    dataDir = "/srv/services/radarr";
+  };
+
+  services.lidarr = {
+    enable = true;
+    dataDir = "/srv/services/lidarr";
+  };
+
+  services.sonarr = {
+    enable = true;
+    dataDir = "/srv/services/sonarr";
+  };
+}
