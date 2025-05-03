444df27957491fd08d1462736e16315d9a130a6d
Landon Buttars <17345308+Buttars@users.noreply.github.com>
2025-05-03 01:06:35 -0600

    refactor(hosts/servus): store torrent management config on the local device instead of NFS

--- hosts/nixos/servus/torrent-media-management.nix
+++ hosts/nixos/servus/torrent-media-management.nix
@@ -1,17 +1,14 @@
 { ... }: {
   services.radarr = {
     enable = true;
-    dataDir = "/srv/services/radarr";
   };
 
   services.lidarr = {
     enable = true;
-    dataDir = "/srv/services/lidarr";
   };
 
   services.sonarr = {
     enable = true;
-    dataDir = "/srv/services/sonarr";
   };
 
   services.prowlarr.enable = true;
