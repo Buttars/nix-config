8ad37b60ab6996a242508075baf53c0b47b87823
Landon Buttars <17345308+Buttars@users.noreply.github.com>
2025-05-29 22:57:03 -0600

    refactor(home/buttars): remove explicitly defined xdg path variables

--- home/buttars/common/features/shell/fish/env-vars.nix
+++ home/buttars/common/features/shell/fish/env-vars.nix
@@ -1,19 +1,11 @@
 { lib, ... }:
 {
-  programs.fish.shellInit = ''
-    set -Ux XDG_CONFIG_HOME $HOME/.config
-    set -Ux XDG_DATA_HOME $HOME/.local/share
-    set -Ux XDG_CACHE_HOME $HOME/.cache
-    set -Ux INPUTRC $XDG_CONFIG_HOME/shell/inputrc
   '';
 
   home.sessionVariables = lib.mkDefault {
     EDITOR = "nvim";
     TERMINAL = "kitty";
     BROWSER = "brave";
-    XDG_CONFIG_HOME = "$HOME/.config";
-    XDG_DATA_HOME = "$HOME/.local/share";
-    XDG_CACHE_HOME = "$HOME/.cache";
     INPUTRC = "$XDG_CONFIG_HOME/shell/inputrc";
   };
 }
