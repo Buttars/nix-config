81394ab3e7c51842251767cbf72151928ab2101c
Landon Buttars <17345308+Buttars@users.noreply.github.com>
2025-05-29 22:54:28 -0600

    fix(home/buttars): sudo aliases causing errors

--- home/buttars/common/features/shell/fish/functions.nix
+++ home/buttars/common/features/shell/fish/functions.nix
@@ -1,24 +1,23 @@
 { ... }:
 {
   programs.fish.interactiveShellInit = ''
-    for command in mount umount sv su shutdown poweroff reboot
-      function $command
-        command sudo $command $argv
-      end
-    end
 
     function cp; command cp -iv $argv; end
     function mv; command mv -iv $argv; end
     function rm; command rm -vI $argv; end
-    function bc; command bc -ql; end
     function rsync; command rsync -vrPlu $argv; end
-    function mkd; command mkdir -pv $argv; end
 
-    function ls; command ls -hN --color=auto --group-directories-first $argv; end
-    function grep; command grep --color=auto $argv; end
-    function diff; command diff --color=auto $argv; end
-    function ccat; command highlight --out-format=ansi $argv; end
-    function ip; command ip -color=auto $argv; end
+    function define_sudo_wrappers --argument-names commands
+      for cmd in $commands
+        set -l def "
+        function $cmd
+            command sudo -- $cmd \$argv
+        end"
+        eval $def
+      end
+    end
+
+    define_sudo_wrappers mount umount shutdown reboot sv su
 
     fish_vi_key_bindings
   '';
