ce069184bad689333408b8d8c89c39f86283a329
Landon Buttars <17345308+Buttars@users.noreply.github.com>
2025-06-20 00:40:52 -0600

    feat(home/buttars-laptop): add fish shell configuration

--- home/buttars/buttars-laptop.nix
+++ home/buttars/buttars-laptop.nix
@@ -3,10 +3,18 @@
   imports = [
     ./common/core
     ./common/features/cli
+    ./common/features/shell/fish
     ./common/features/desktop/common
     ./common/features/desktop/hyprland
     #./common/features/sops.nix
     ./common/features/desktop/common/browser.nix
     ./common/features/services/syncthing.nix
   ];
+
+  home.sessionVariables = {
+    GDK_SCALE = "2";
+    GDK_DPI_SCALE = "0.5"; # Optional fine-tuning
+    XCURSOR_SIZE = "48"; # Larger cursor to match scale
+  };
+
 }
