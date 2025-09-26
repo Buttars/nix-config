daaa3183220366b734871064d235a20bd9e2b5a4
Buttars <17345308+Buttars@users.noreply.github.com>
2025-09-26 09:44:53 -0600

    feat(home/wgu): configure git to use personal credentials for personal projects

--- home/wgu/common/core/git.nix
+++ home/wgu/common/core/git.nix
@@ -7,5 +7,22 @@
     enable = true;
     userName = "Landon Buttars";
     userEmail = "landon.buttars@wgu.edu";
+
+    includes = [
+      {
+        condition = "hasconfig:remote.*.url:git@github.com-buttars:";
+        contents.user = {
+          name = "Landon Buttars";
+          email = "17345308+Buttars@users.noreply.github.com";
+        };
+      }
+      {
+        condition = "hasconfig:remote.*.url:git@github.com-landon-buttars-wgu:";
+        contents.user = {
+          name = "Landon Buttars";
+          email = "landon.buttars@wgu.edu";
+        };
+      }
+    ];
   };
 }
