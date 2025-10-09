7625dfcd45bd741b6550a7776de2f539fc1e0963
Buttars <17345308+Buttars@users.noreply.github.com>
2025-10-09 15:47:31 -0600

    fix(flake): remove redundant overlays in darwin configuration

--- flake.nix
+++ flake.nix
@@ -78,9 +78,6 @@
             {
               nixpkgs.overlays = [
                 inputs.darwin.overlays.default
-                inputs.self.overlays.additions
-                inputs.self.overlays.modifications
-                inputs.self.overlays.unstable-packages
               ];
             }
           )
