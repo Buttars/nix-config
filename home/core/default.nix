bc60e4f49244c73c37508eaa7b149607c9b10c56
Landon Buttars <17345308+Buttars@users.noreply.github.com>
2025-10-22 08:31:21 -0600

    feat(home/core): add nil nix formatter

--- home/core/default.nix
+++ home/core/default.nix
@@ -1,9 +1,12 @@
-{ stateVersion, ... }:
-{
+{ stateVersion, pkgs, ... }: {
   imports = [
     ./cli.nix
     ./git.nix
     ./neovim.nix
+  ];
+
+  home.packages = with pkgs; [
+    nil
   ];
 
   home.stateVersion = stateVersion;
