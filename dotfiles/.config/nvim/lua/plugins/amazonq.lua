ff21c05ea5e12717d00b78c1db6f65c622317dc4
Buttars <17345308+Buttars@users.noreply.github.com>
2025-11-11 11:49:13 -0700

    refactor(dotfiles): disable amazonq due to region select bug

--- dotfiles/.config/nvim/lua/plugins/amazonq.lua
+++ dotfiles/.config/nvim/lua/plugins/amazonq.lua
@@ -1,15 +1,15 @@
 return {
-  {
-    name = "amazonq",
-    url = "https://github.com/awslabs/amazonq.nvim.git",
-    config = function()
-      require("amazonq").setup({
-        debug = true,
-        ssoStartUrl = os.getenv("AMAZONQ_START_URL") or "https://view.awsapps.com/start",
-      })
-    end,
-    opts = {
-      ssoStartUrl = os.getenv("AMAZONQ_START_URL") or "https://view.awsapps.com/start",
-    },
-  },
+  -- {
+  --   name = "amazonq",
+  --   url = "https://github.com/awslabs/amazonq.nvim.git",
+  --   config = function()
+  --     require("amazonq").setup({
+  --       debug = true,
+  --       ssoStartUrl = os.getenv("AMAZONQ_START_URL") or "https://view.awsapps.com/start",
+  --     })
+  --   end,
+  --   opts = {
+  --     ssoStartUrl = os.getenv("AMAZONQ_START_URL") or "https://view.awsapps.com/start",
+  --   },
+  -- },
 }
