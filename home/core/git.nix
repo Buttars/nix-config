93c2c8154c36af12641956fd6528e048d48fcb85
Buttars <17345308+Buttars@users.noreply.github.com>
2025-11-13 20:09:24 -0700

    feat(home/core): set git pager to diffnav, update configuration to new spec

--- home/core/git.nix
+++ home/core/git.nix
@@ -2,15 +2,16 @@
 {
   programs.git = {
     enable = true;
-    delta.enable = true;
     lfs.enable = true;
-    extraConfig = {
+    settings = {
       log.decorate = "short";
       log.abbrevCommit = "true";
       log.format = "oneline";
       format.pretty = "oneline";
       push.autoSetupRemote = true;
       pull.rebase = true;
+      core.pager = "delta";
+      pager.diff = "diffnav";
     };
   };
 }
