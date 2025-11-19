2d5d2c02dd043a5c1925e072f4b0c4c756a04b42
Landon Buttars <17345308+Buttars@users.noreply.github.com>
2025-11-18 22:01:00 -0700

    checkin

--- hosts/nixos/buttars-desktop/default.nix
+++ hosts/nixos/buttars-desktop/default.nix
@@ -1,4 +1,4 @@
-{ hostname, ... }:
+{ pkgs, hostname, ... }:
 {
   imports = [
     ./hardware-configuration.nix
@@ -15,17 +15,51 @@
   ];
 
   stylix.enable = true;
-  stylix.autoEnable = false;
-  stylix = {
-    base16Scheme = builtins.fetchurl {
-      url = "https://raw.githubusercontent.com/scottmckendry/cyberdream.nvim/main/extras/base16/cyberdream.yaml";
-      sha256 = "1bfi479g7v5cz41d2s0lbjlqmfzaah68cj1065zzsqksx3n63znf";
+  stylix.autoEnable = true;
+  # stylix = {
+  #   base16Scheme = builtins.fetchurl {
+  #     url = "https://raw.githubusercontent.com/scottmckendry/cyberdream.nvim/main/extras/base16/cyberdream.yaml";
+  #     sha256 = "1bfi479g7v5cz41d2s0lbjlqmfzaah68cj1065zzsqksx3n63znf";
+  #   };
+  #   override = {
+  #     base00 = "#0F0F11";
+  #     base0E = "#DE4F72";
+  #   };
+  # };
+
+  stylix.fonts = {
+    sansSerif = {
+      package = pkgs.inter-nerdfont;
+      name = "Inter Nerd Font";
     };
-    override = {
-      base00 = "#0F0F11";
-      base0E = "#DE4F72";
+
+    monospace = {
+      package = pkgs.nerd-fonts.commit-mono;
+      name = "CommitMono Nerd Font Mono";
     };
   };
+
+  stylix.base16Scheme = {
+    name = "Tokyo Neon Cyber";
+    base00 = "0F0F11";
+    base01 = "0C141D";
+    base02 = "15161A";
+    base03 = "2E2E2E";
+    base04 = "A0A0A0";
+    base05 = "D9D7D6";
+    base06 = "E0DEDC";
+    base07 = "A3FFFF";
+    base08 = "FF3B7F";
+    base09 = "FF9933";
+    base0A = "FCEE0C";
+    base0B = "19FF92";
+    base0C = "19F9FF";
+    base0D = "00F0FF";
+    base0E = "FF2EA6";
+    base0F = "FF6EC7";
+  };
+
+  # stylix.targets.hyprland.enable = true;
 
   programs.dconf.enable = true;
 
