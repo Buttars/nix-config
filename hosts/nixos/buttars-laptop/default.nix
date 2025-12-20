b8c888706208f30715295a9d74329e3d180688c7
Landon Buttars <17345308+Buttars@users.noreply.github.com>
2025-12-19 23:25:06 -0700

    feat(hosts/buttars-laptop): configure stylix, will be moved to a shared feature

--- hosts/nixos/buttars-laptop/default.nix
+++ hosts/nixos/buttars-laptop/default.nix
@@ -11,6 +11,19 @@
     ../../common/features/nixos/fonts.nix
   ];
 
+  stylix.enable = true;
+  stylix.autoEnable = false;
+  stylix = {
+    base16Scheme = builtins.fetchurl {
+      url = "https://raw.githubusercontent.com/scottmckendry/cyberdream.nvim/main/extras/base16/cyberdream.yaml";
+      sha256 = "1bfi479g7v5cz41d2s0lbjlqmfzaah68cj1065zzsqksx3n63znf";
+    };
+    override = {
+      base00 = "#0F0F11";
+      base0E = "#DE4F72";
+    };
+  };
+
   programs.dconf.enable = true;
 
   programs.hyprland.enable = true;
