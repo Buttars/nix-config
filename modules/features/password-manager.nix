5c7c5a581cd389481dbb2079115350d7336a4f6a
Landon Buttars <17345308+Buttars@users.noreply.github.com>
2026-02-11 15:28:01 -0700

    feat: add password-manager feature

---
+++ modules/features/password-manager.nix
@@ -0,0 +1,6 @@
+{
+  features.password-manager = {
+    _.keepassxc.homeManger = { pkgs, ... }: { home.packages = with pkgs; [ keepassxc ]; };
+    _.bitwarden.homeManager = { pkgs, ... }: { home.packages = with pkgs; [ bitwarden-cli bitwarden-desktop ]; };
+  };
+}
