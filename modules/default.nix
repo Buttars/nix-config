d6bc6b86d315812f6ca7b51b48fe7f07032e25de
Landon Buttars <17345308+Buttars@users.noreply.github.com>
2026-02-18 14:19:21 -0700

    feat: set configuration defaults

---
+++ modules/default.nix
@@ -0,0 +1,58 @@
+{ inputs, __findFile, ... }:
+{
+  den.default = {
+    includes = [
+      <den/define-user>
+      <den/home-manager>
+      (
+        { host, ... }:
+        {
+          ${host.class}.networking.hostName = host.name;
+        }
+      )
+    ];
+
+    nixos = {
+      documentation.doc.enable = false;
+      documentation.info.enable = false;
+      i18n.defaultLocale = "en_US.UTF-8";
+      i18n.supportedLocales = [ "all" ];
+      system.stateVersion = "25.11";
+      time.timeZone = "America/Denver";
+      home-manager.useUserPackages = true;
+      home-manager.useGlobalPkgs = true;
+      boot.initrd.systemd.enable = true;
+    };
+
+    darwin = {
+      imports = [
+        {
+          home-manager.useUserPackages = true;
+          home-manager.useGlobalPkgs = true;
+          nixpkgs.overlays = with inputs; [
+            darwin.overlays.default
+            self.overlays.additions
+            self.overlays.modifications
+          ];
+        }
+      ];
+      nix.enable = false;
+      system.stateVersion = 5;
+      nixpkgs.config.allowUnfree = true;
+    };
+
+    homeManager = {
+      programs.home-manager.enable = true;
+      home = {
+        sessionPath = [ "$HOME/.local/bin" ];
+        sessionVariables.NIXPKGS_ALLOW_UNFREE = "1";
+        stateVersion = "22.11";
+      };
+    };
+  };
+
+  flake-file.inputs.home-manager = {
+    url = "github:nix-community/home-manager";
+    inputs.nixpkgs.follows = "nixpkgs";
+  };
+}
