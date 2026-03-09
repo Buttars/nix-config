a000011e9e7e15bc2132dc1f6f57f21cfecfd7f8
Landon Buttars <17345308+Buttars@users.noreply.github.com>
2026-03-09 17:06:36 -0600

    feat(zsa): add ZSA keyboard configuration and keymaps

---
+++ modules/features/zsa.nix
@@ -0,0 +1,11 @@
+{
+  aegis.zsa.nixos =
+    { pkgs, ... }:
+    {
+      hardware.keyboard.zsa.enable = true;
+
+      environment.systemPackages = with pkgs; [
+        wally-cli
+      ];
+    };
+}
