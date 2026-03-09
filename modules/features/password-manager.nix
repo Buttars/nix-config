94c60a3241226a75de6527acd04d3c28807c21c1
Landon Buttars <17345308+Buttars@users.noreply.github.com>
2026-03-09 17:06:36 -0600

    feat(password-manager): add password manager integration and defaults

---
+++ modules/features/password-manager.nix
@@ -0,0 +1,17 @@
+{
+  aegis.password-manager = {
+    _.keepassxc.homeManger =
+      { pkgs, ... }:
+      {
+        home.packages = with pkgs; [ keepassxc ];
+      };
+    _.bitwarden.homeManager =
+      { pkgs, ... }:
+      {
+        home.packages = with pkgs; [
+          bitwarden-cli
+          bitwarden-desktop
+        ];
+      };
+  };
+}
