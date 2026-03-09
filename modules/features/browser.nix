1ee5579f57e648f6419ae5c69370689ee7a76177
Landon Buttars <17345308+Buttars@users.noreply.github.com>
2026-03-09 17:06:36 -0600

    feat(browser): configure preferred web browser and settings

---
+++ modules/features/browser.nix
@@ -0,0 +1,18 @@
+{
+  aegis.browser = {
+    _.google-chrome.homeManager =
+      { pkgs, ... }:
+      {
+        home.packages = with pkgs; [
+          google-chrome
+        ];
+      };
+    _.brave.homeManager =
+      { pkgs, ... }:
+      {
+        home.packages = with pkgs; [
+          brave
+        ];
+      };
+  };
+}
