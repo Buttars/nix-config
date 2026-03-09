0fbfc6c9c9b6f0e6412de7fb751a51480392cf29
Landon Buttars <17345308+Buttars@users.noreply.github.com>
2026-03-09 17:06:36 -0600

    feat(xdg): configure xdg user directories and defaults

---
+++ modules/features/xdg.nix
@@ -0,0 +1,21 @@
+{
+  # XDG user dirs and utilities for Home Manager
+  # Included by: home/* (migrate from home/features/xdg.nix)
+
+  aegis.xdg = {
+    homeManager =
+      { pkgs, ... }:
+      {
+        home.packages = with pkgs; [
+          xdg-utils
+          xdg-user-dirs
+        ];
+
+        xdg = {
+          enable = true;
+          userDirs.enable = true;
+          userDirs.createDirectories = true;
+        };
+      };
+  };
+}
