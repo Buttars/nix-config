258644716038f022e521f611752279e37d86d438
Landon Buttars <17345308+Buttars@users.noreply.github.com>
2026-03-09 17:06:36 -0600

    feat(fish): add Fish shell configuration and plugins

---
+++ modules/features/fish.nix
@@ -0,0 +1,28 @@
+{
+  aegis.fish = {
+    programs.fish = {
+      enable = true;
+      vendor = {
+        completions.enable = true;
+        config.enable = true;
+        functions.enable = true;
+      };
+    };
+
+    homeManager =
+      { pkgs, ... }:
+      {
+        programs.fish = {
+          enable = true;
+
+          shellInit = ''
+            set fish_greeting
+          '';
+
+          loginShellInit = "";
+          interactiveShellInit = "";
+          shellInitLast = "";
+        };
+      };
+  };
+}
