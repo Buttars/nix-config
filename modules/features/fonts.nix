4dc55cba292c13fb2b201d8481f4303249ee432c
Landon Buttars <17345308+Buttars@users.noreply.github.com>
2026-03-09 17:06:36 -0600

    feat(fonts): install and configure system fonts

---
+++ modules/features/fonts.nix
@@ -0,0 +1,28 @@
+{
+  aegis.fonts.nixos =
+    { pkgs, ... }:
+    {
+
+      environment.systemPackages = with pkgs; [
+        noto-fonts
+        noto-fonts-cjk-sans
+        noto-fonts-color-emoji
+
+        font-manager
+      ];
+
+      fonts.packages = with pkgs; [
+        nerd-fonts.sauce-code-pro
+        nerd-fonts.noto
+        nerd-fonts.inconsolata
+        nerd-fonts.roboto-mono
+        nerd-fonts.commit-mono
+
+        dejavu_fonts
+        noto-fonts
+        cantarell-fonts
+        inter
+        inter-nerdfont
+      ];
+    };
+}
