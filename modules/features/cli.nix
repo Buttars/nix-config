7f3890f617986df723ebfc6b12e84aac8204cd7b
Landon Buttars <17345308+Buttars@users.noreply.github.com>
2026-03-09 17:06:36 -0600

    feat(cli): add command-line tooling configuration

---
+++ modules/features/cli.nix
@@ -0,0 +1,89 @@
+{
+  aegis.cli = {
+    homeManager =
+      { lib, pkgs, ... }:
+      {
+        home.packages =
+          with pkgs;
+          [
+            bat
+            eza
+            fd
+            intelli-shell
+            nix-search-tv
+            tldr
+            watch
+            wikiman
+            zoxide
+          ]
+          ++ lib.optionals pkg.stdenv.isLinux [
+            trashy
+          ];
+
+        programs.direnv = {
+          enable = true;
+          nix-direnv.enable = true;
+        };
+      };
+
+    _.tui = {
+      homeManager =
+        { pkgs, ... }:
+        {
+          home.packages = with pkgs; [
+            tmux
+            btop
+            diffnav
+            gh
+            gh-dash
+            htop
+            process-compose
+            sesh
+            sshs
+            yazi
+          ];
+          programs.fzf = {
+            enable = true;
+            defaultOptions = [ "--color 16" ];
+          };
+        };
+    };
+
+    _.aws = {
+      homeManager =
+        { pkgs, ... }:
+        {
+          home.packages = with pkgs; [
+            awscli2
+            terraform
+          ];
+        };
+
+    };
+
+    _.git = {
+      homeManager =
+        { pkgs, ... }:
+        {
+          home.packages = with pkgs; [
+            delta
+            diffnav
+          ];
+          programs.git = {
+            enable = true;
+            lfs.enable = true;
+            settings = {
+              log.decorate = "short";
+              log.abbrevCommit = "true";
+              log.format = "oneline";
+              format.pretty = "oneline";
+              push.autoSetupRemote = true;
+              pull.rebase = true;
+              core.pager = "delta";
+              pager.diff = "diffnav";
+            };
+          };
+        };
+    };
+  };
+}
