daae49967312af663409b9bc4ed505f184707c76
Landon Buttars <17345308+Buttars@users.noreply.github.com>
2026-03-09 17:06:36 -0600

    feat(nvidia): enable nvidia drivers and helper configuration

---
+++ modules/features/nvidia.nix
@@ -0,0 +1,22 @@
+{
+  aegis.nvidia.nixos =
+    { config, ... }:
+    {
+      hardware.graphics = {
+        enable = true;
+      };
+
+      services.xserver.videoDrivers = [ "nvidia" ];
+
+      hardware.nvidia-container-toolkit.enable = true;
+
+      hardware.nvidia = {
+        modesetting.enable = true;
+        powerManagement.enable = true;
+        powerManagement.finegrained = false;
+        nvidiaSettings = true;
+        open = true;
+        package = config.boot.kernelPackages.nvidiaPackages.production;
+      };
+    };
+}
