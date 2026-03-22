f632d54693d52fe7c659752a38e0988ff79b0f34
Landon Buttars <17345308+Buttars@users.noreply.github.com>
2026-03-22 00:52:45 -0600

    feat(host/sentinel): add sentinel host

--- modules/hosts/sentinel/defualt.nix
+++ modules/hosts/sentinel/defualt.nix
@@ -7,16 +7,35 @@
     includes = [
       <den/define-user>
       <aegis/networking>
-      # (<aegis/disks/btrfs> {
-      #   disk = "/dev/sda";
-      #   withSwap = true;
-      #   swapSize = "32";
-      # })
+      <aegis/secrets>
     ];
 
-    nixos = {
-      services.openssh.enable = true;
-    };
+    nixos =
+      { config, ... }:
+      {
+        imports = [ ./_disko.nix ];
+
+        hardware.facter.reportPath = ./facter.json;
+
+        sops.secrets.buttars-password.neededForUsers = true;
+
+        users.mutableUsers = false;
+        users.users.sentinel.hashedPasswordFile = config.sops.secrets.buttars-password.path;
+        users.users.sentinel.extraGroups = [ "wheel" ];
+        users.users.sentinel.openssh.authorizedKeys.keyFiles = [ ../../users/buttars/id_ed25519.pub ];
+
+        services.openssh.enable = true;
+        services.btrfs.autoScrub.enable = true;
+        services.beesd.filesystems.nixroot = {
+          spec = "/";
+          hashTableSizeMB = 256;
+          verbosity = "crit";
+          extraOptions = [
+            "--loadavg-target"
+            "2.0"
+          ];
+        };
+      };
 
     homeManager =
       { pkgs, ... }:
