6654dbe0584549994934e9aaec0ce82c79bb32c3
Landon Buttars <17345308+Buttars@users.noreply.github.com>
2026-03-26 13:25:44 -0600

    feat(flake): add nixpkgs input to flake-file config

--- modules/default.nix
+++ modules/default.nix
@@ -62,6 +62,8 @@
   };
 
   flake-file.inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
