1b9e00e23cbfe0f95e4e9894febed52ea27c3792
Landon Buttars <17345308+Buttars@users.noreply.github.com>
2026-03-27 03:18:53 -0600

    feat(features/niri): add niri compositor aspect

---
+++ modules/features/niri.nix
@@ -0,0 +1,25 @@
+{
+  aegis.niri = {
+    nixos = {
+      programs.niri.enable = true;
+      environment.pathsToLink = [
+        "/share/applications"
+        "/share/xdg-desktop-portal"
+      ];
+    };
+    homeManager =
+      { pkgs, ... }:
+      {
+        home.packages = with pkgs; [
+          fuzzel
+          grim
+          libnotify
+          slurp
+          swaynotificationcenter
+          waybar
+          wl-clipboard
+          xdg-desktop-portal-gnome
+        ];
+      };
+  };
+}
