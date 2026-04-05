fe17e5a2b62a8c14f50810ecaaab45b96a0c1c3c
Landon Buttars <17345308+Buttars@users.noreply.github.com>
2026-04-04 21:29:18 -0600

    feat(host/sentinel): add caddy reverse proxy for homelab services

--- modules/hosts/sentinel/default.nix
+++ modules/hosts/sentinel/default.nix
@@ -1,7 +1,10 @@
 { __findFile, ... }:
 {
   den.hosts.x86_64-linux.sentinel = {
-    users.sentinel.classes = [ "home-manager" ];
+    users.sentinel = {
+      classes = [ "homeManager" ];
+      aspect = "sentinel-user";
+    };
   };
   den.aspects.sentinel = {
     includes = [
@@ -16,15 +19,85 @@
         imports = [ ./_disko.nix ];
 
         hardware.facter.reportPath = ./facter.json;
+        hardware.facter.detected.dhcp.interfaces = [ "ens18" ];
 
         sops.secrets.buttars-password.neededForUsers = true;
 
         users.mutableUsers = false;
         users.users.sentinel.hashedPasswordFile = config.sops.secrets.buttars-password.path;
         users.users.sentinel.extraGroups = [ "wheel" ];
+        users.users.sentinel.createHome = true;
+        systemd.tmpfiles.rules = [
+          "d /home/sentinel/.ssh 0700 sentinel users -"
+        ];
+        sops.secrets."private_keys/sentinel" = {
+          owner = "sentinel";
+          path = "/home/sentinel/.ssh/id_ed25519";
+          mode = "0600";
+        };
         users.users.sentinel.openssh.authorizedKeys.keyFiles = [ ../../users/buttars/keys/id_ed25519.pub ];
 
         services.openssh.enable = true;
+
+        services.caddy = {
+          enable = true;
+          email = "admin@buttars.dev";
+          virtualHosts = {
+            "http://jellyfin.buttars.lan".extraConfig = "reverse_proxy http://theatrum.lan:8096";
+            "jellyfin.buttars.dev".extraConfig = ''
+              tls {
+                protocols tls1.2 tls1.3
+              }
+              reverse_proxy http://theatrum.lan:8096
+            '';
+
+            "http://qbittorrent.buttars.lan".extraConfig = "reverse_proxy http://torrens.lan:8080";
+            "qbittorrent.buttars.dev".extraConfig = ''
+              tls {
+                protocols tls1.2 tls1.3
+              }
+              reverse_proxy http://torrens.lan:8080
+            '';
+
+            "http://radarr.buttars.lan".extraConfig = "reverse_proxy http://torrens.lan:7878";
+            "radarr.buttars.dev".extraConfig = ''
+              tls {
+                protocols tls1.2 tls1.3
+              }
+              reverse_proxy http://torrens.lan:7878
+            '';
+
+            "http://sonarr.buttars.lan".extraConfig = "reverse_proxy http://torrens.lan:8989";
+            "sonarr.buttars.dev".extraConfig = ''
+              tls {
+                protocols tls1.2 tls1.3
+              }
+              reverse_proxy http://torrens.lan:8989
+            '';
+
+            "http://lidarr.buttars.lan".extraConfig = "reverse_proxy http://torrens.lan:8686";
+            "lidarr.buttars.dev".extraConfig = ''
+              tls {
+                protocols tls1.2 tls1.3
+              }
+              reverse_proxy http://torrens.lan:8686
+            '';
+
+            "http://prowlarr.buttars.lan".extraConfig = "reverse_proxy http://torrens.lan:9696";
+            "prowlarr.buttars.dev".extraConfig = ''
+              tls {
+                protocols tls1.2 tls1.3
+              }
+              reverse_proxy http://torrens.lan:9696
+            '';
+          };
+        };
+
+        networking.firewall.allowedTCPPorts = [
+          80
+          443
+        ];
+
         services.btrfs.autoScrub.enable = true;
         services.beesd.filesystems.nixroot = {
           spec = "/";
