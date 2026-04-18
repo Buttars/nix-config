2033fdce893aca8199f073b962c5c456113fbc01
Landon Buttars <17345308+Buttars@users.noreply.github.com>
2026-04-18 01:59:26 -0600

    fix(features/hyprland): hyprland splash and portal warnings shown on startup

--- modules/features/hyprland/default.nix
+++ modules/features/hyprland/default.nix
@@ -84,7 +84,6 @@
             misc = {
               disable_hyprland_logo = true;
               disable_splash_rendering = true;
-              suppress_portal_warnings = true;
               on_focus_under_fullscreen = true;
             };
 
