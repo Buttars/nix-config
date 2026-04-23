16bc510922659fa7635347e2d8ae7123292a5c79
Landon Buttars <17345308+Buttars@users.noreply.github.com>
2026-04-22 21:23:14 -0600

    refactor(users/buttars): replace dotfiles symlinks with nix-managed config

--- modules/users/buttars/dotfiles.nix
+++ modules/users/buttars/dotfiles.nix
@@ -5,9 +5,7 @@
       inherit (inputs) dotfiles;
     in
     {
-      pkgs,
       config,
-      lib,
       ...
     }:
     {
@@ -16,26 +14,8 @@
           source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nix-config/dendritic-from-scratch/dotfiles/.config/nvim";
           recursive = true;
         };
-        ".config/shell" = {
-          source = "${dotfiles}/.config/shell";
-          recursive = true;
-        };
-
-        # ".config/hypr".source = "${dotfiles}/.config/hypr";
-        ".config/lf".source = "${dotfiles}/.config/lf";
-        ".config/zsh".source = "${dotfiles}/.config/zsh";
-        ".config/alacritty".source = "${dotfiles}/.config/alacritty";
-        ".config/kitty".source = "${dotfiles}/.config/kitty";
         ".config/rofi".source = "${dotfiles}/.config/rofi";
         ".config/waybar".source = "${dotfiles}/.config/waybar";
-        ".zprofile".source = "${dotfiles}/.config/shell/profile";
-        ".config/nixpkgs/config.nix" = {
-          text = ''
-            {
-              allowUnfree = true;
-            }
-          '';
-        };
       };
     };
 }
