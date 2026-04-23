b9fe3eeef0aea5cd5f78a15452361f289523d76e
Landon Buttars <17345308+Buttars@users.noreply.github.com>
2026-04-22 21:28:43 -0600

    feat(features/fish): add aliases subaspect with common aliases and functions

--- modules/features/fish.nix
+++ modules/features/fish.nix
@@ -27,8 +27,44 @@
         };
       };
 
-    # Per-user fish tweaks (aliases, functions, env, sesh, graphical-session) have
-    # been moved to modules/users/buttars/fish.nix so they can be applied only for
-    # the `buttars` aspect. Keep this file focused on the base fish configuration.
+    _.aliases = {
+      homeManager =
+        { lib, pkgs, ... }:
+        {
+          programs.fish = {
+            shellAliases = {
+              ll = "ls -la";
+              ka = "killall";
+              g = "git";
+              e = "$EDITOR";
+              v = "$EDITOR";
+            }
+            // lib.optionalAttrs pkgs.stdenv.isLinux {
+              sdn = "shutdown -h now";
+            };
+
+            interactiveShellInit = ''
+              function cp; command cp -iv $argv; end
+              function mv; command mv -iv $argv; end
+              function rm; command rm -vI $argv; end
+              function rsync; command rsync -vrPlu $argv; end
+
+              function define_sudo_wrappers --argument-names commands
+                for cmd in $commands
+                  set -l def "
+                  function $cmd
+                      command sudo -- $cmd \$argv
+                  end"
+                  eval $def
+                end
+              end
+
+              define_sudo_wrappers mount umount shutdown reboot sv su
+
+              fish_vi_key_bindings
+            '';
+          };
+        };
+    };
   };
 }
