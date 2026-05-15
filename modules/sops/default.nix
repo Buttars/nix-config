d1e0dea4135a63d2def7aebd6b2b9fc39383f3d8
Landon Buttars <17345308+Buttars@users.noreply.github.com>
2026-05-15 17:42:17 -0600

    feat(sops): add darwin support for age key paths

--- modules/sops/default.nix
+++ modules/sops/default.nix
@@ -1,28 +1,49 @@
 { inputs, ... }:
-let
-  sops_config = {
-    defaultSopsFile = ./secrets.yaml;
-    defaultSopsFormat = "yaml";
-    age = {
-      keyFile = "/var/lib/secrets/sops/age/keys.txt";
-      sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
-      generateKey = true;
-    };
-  };
-in
 {
   aegix.sops = {
     homeManager =
-      { pkgs, ... }:
+      {
+        pkgs,
+        lib,
+        config,
+        ...
+      }:
+      let
+        keyFile =
+          if pkgs.stdenv.isDarwin then
+            "${config.home.homeDirectory}/.config/sops/age/keys.txt"
+          else
+            "/var/lib/secrets/sops/age/keys.txt";
+      in
       {
         imports = [ inputs.sops-nix.homeManagerModules.sops ];
-        sops = sops_config;
-        home.sessionVariables.SOPS_AGE_KEY_FILE = sops_config.age.keyFile;
+        sops = {
+          defaultSopsFile = ./secrets.yaml;
+          defaultSopsFormat = "yaml";
+          age = {
+            inherit keyFile;
+            sshKeyPaths =
+              if pkgs.stdenv.isDarwin then
+                [ "${config.home.homeDirectory}/.ssh/id_ed25519" ]
+              else
+                [ "/etc/ssh/ssh_host_ed25519_key" ];
+            generateKey = !pkgs.stdenv.isDarwin;
+          };
+        };
+        home.sessionVariables.SOPS_AGE_KEY_FILE = keyFile;
         home.packages = [ pkgs.sops ];
       };
     nixos = {
       imports = [ inputs.sops-nix.nixosModules.sops ];
-      sops = sops_config;
+      sops = {
+        defaultSopsFile = ./secrets.yaml;
+        defaultSopsFormat = "yaml";
+        age = {
+          keyFile = "/var/lib/secrets/sops/age/keys.txt";
+          sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
+          generateKey = true;
+        };
+      };
     };
   };
 
