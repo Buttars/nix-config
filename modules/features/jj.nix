4682d48d8cc6c23ad902096d55f4303e4244bbaf
Buttars <17345308+Buttars@users.noreply.github.com>
2026-06-03 16:07:12 -0600

    feat(features/jj): add neovim as diff editor

--- modules/features/jj.nix
+++ modules/features/jj.nix
@@ -5,7 +5,14 @@
       {
         programs.jujutsu = {
           enable = true;
-          settings.git.colocate = true;
+          settings = {
+            git.colocate = true;
+            ui.diff-editor = "vimdiff";
+            merge-tools.vimdiff = {
+              program = "nvim";
+              diff-args = ["-d" "$left" "$right"];
+            };
+          };
         };
       };
   };
