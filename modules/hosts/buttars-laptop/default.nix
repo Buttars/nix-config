9763c7da1de84a96341bb6b24f7c6b1e7e466623
Landon Buttars <17345308+Buttars@users.noreply.github.com>
2026-06-09 20:57:03 -0600

    fix(host/buttars-laptop): monitor and environment settings not applied in lua mode

--- modules/hosts/buttars-laptop/default.nix
+++ modules/hosts/buttars-laptop/default.nix
@@ -71,13 +71,14 @@
 
       home-manager.users.buttars = {
         wayland.windowManager.hyprland.settings = {
-          monitor = [ "eDP-1, 1920x1080@60, 0x0, 1" ];
-          env = [
-            "GDK_SCALE,1"
-            "QT_SCALE_FACTOR,1"
-            "QT_AUTO_SCREEN_SCALE_FACTOR,0"
+          monitor = [
+            {
+              output = "eDP-1";
+              mode = "1920x1080@60";
+              position = "0x0";
+              scale = 1;
+            }
           ];
-          xwayland.force_zero_scaling = true;
         };
       };
 
