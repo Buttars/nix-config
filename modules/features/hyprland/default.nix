8423d28898cd36b67f35645f668072a34bba7455
Landon Buttars <17345308+Buttars@users.noreply.github.com>
2026-08-19 21:34:14 -0600

    feat(features/hyprland): disable focus-follows-mouse

--- modules/features/hyprland/default.nix
+++ modules/features/hyprland/default.nix
@@ -91,7 +91,7 @@
                 input = {
                     kb_layout     = "us",
                     kb_options    = "ctrl:nocaps",
-                    follow_mouse  = 1,
+                    follow_mouse  = 0,
                     sensitivity   = 0,
                     accel_profile = "flat",
                     touchpad = {
