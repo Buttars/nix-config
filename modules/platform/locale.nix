a6d4849035289956b719bfb278353a7476b8bb78
Buttars <17345308+Buttars@users.noreply.github.com>
2026-09-01 15:12:40 -0600

    fix(platform/locale): set UTF-8 LANG on darwin (home-manager)

--- modules/platform/locale.nix
+++ modules/platform/locale.nix
@@ -10,4 +10,11 @@
       };
       time.timeZone = lib.mkDefault "America/Denver";
     };
+
+  aegix.locale.homeManager = {
+    home.sessionVariables = {
+      LANG = "en_US.UTF-8";
+      LC_ALL = "en_US.UTF-8";
+    };
+  };
 }
